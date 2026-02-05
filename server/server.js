c85c9345828938a7ca1d07f74facbe5121129f04
Ayush <ayushg1500@gmail.com>
2026-02-06 01:02:58 +0530

    api routes added

--- server/server.js
+++ server/server.js
@@ -1,21 +1,51 @@
 import express from 'express'
 import dotenv from "dotenv"
 import { connectionDB } from './config/db.js'
-const app =express()
+import Product from './models/product.model.js'
+const app = express()
 dotenv.config()
 
 
-app.listen(5000, ()=>{
+app.use(express.json()) // this is middleware
+/*
+➡️ Reads the incoming request body
+➡️ Parses JSON data
+➡️ Converts it into a JavaScript object
+➡️ Attaches it to req.body
+*/
+
+app.listen(5000, () => {
     connectionDB()
-console.log("Server started at port 5000")
+    console.log("Server started at port 5000")
 })
 
 
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     res.send("This is home page hello")
 })
 
-app.get("/products", (req, res)=>{
-    
-    res.send("Server is ready, here are the products..")
+app.post("/api/products", async (req, res) => {
+
+    const product = req.body;
+
+    if (!product.name || !product.price || !product.image) {
+        return res.status(400).json({ success: false, message: "Please provide all fields" })
+    }
+
+    const newProduct = new Product(product)
+    try {
+        await newProduct.save()
+        res.status(201).json({ success: true, data: newProduct })
+    }
+    catch (error) {
+        console.error("Error in create product:", error.message)
+        res.status(500).json({ success: false, message: "Server Error" })
+    }
+})
+
+app.delete("/api/products/:id", async (req, res) => {
+
+    const {id} = req.params
+    console.log(id)
+
 })
