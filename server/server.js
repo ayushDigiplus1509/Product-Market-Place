afe5c2e3b39c858413557143fce11bf3ef51bbd8
ayushDigiplus1509 <ayushg.digiplus@gmail.com>
2026-02-10 12:07:59 +0530

    updaqte method added

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from "dotenv"
 import { connectionDB } from './config/db.js'
 import Product from './models/product.model.js'
+import mongoose from 'mongoose'
 const app = express()
 dotenv.config()
 
@@ -24,8 +25,18 @@ app.get("/", (req, res) => {
     res.send("This is home page hello")
 })
 
-app.post("/api/products", async (req, res) => {
+app.get("/api/products", async (req, res) => {
+    // this will return all the products present in the database
+    try {
+        const products = await Product.find()
+        res.status(200).json({ success: true, message: products })
+    } catch (error) {
+        res.status(500).json({ success: false, message: "Server Error" })
+    }
+})
 
+app.post("/api/products", async (req, res) => {
+    // this will add product to the database
     const product = req.body;
 
     if (!product.name || !product.price || !product.image) {
@@ -45,16 +56,36 @@ app.post("/api/products", async (req, res) => {
 })
 
 app.delete("/api/products/:id", async (req, res) => {
-
-    const {id} = req.params
-    try{
+    // this will find and delete a particular product by id
+    const { id } = req.params
+    try {
         await Product.findByIdAndDelete(id)
         res.status(200).json()
     }
-    catch(error){
-        console.error("Error in deleting product",error.message)
-        res.status(500).json({success: false, message:error.message})
+    catch (error) {
+        console.error("Error in deleting product", error.message)
+        res.status(404).json({ success: false, message: "Product not found" })
     }
     console.log(id)
+
+})
+
+app.put("/api/products/:id", async (req, res) => {
+    // this will update the product in database
+    const { id } = req.params
+    const product = req.body
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({success: false, message: "No such product exist"})
+    }
+
+    try {
+        const updatedProduct = await Product.findByIdAndUpdate(id, product, { new: true })
+        res.status(200).json({success: true, data: updatedProduct})
+    } catch (error) {
+        res.status(500).json({success: false, message:"Server error"})
+    }
+
+
 
 })
