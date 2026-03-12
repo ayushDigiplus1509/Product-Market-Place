63eb13997eb70575adf24053426ad4da6fe243df
ayushDigiplus1509 <ayushg.digiplus@gmail.com>
2026-03-12 16:10:30 +0530

    new components added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,29 @@
+import { Container, Flex } from "@chakra-ui/react";
+
 export default function Navbar() {
   return (
-    <div>
-      <h1>This is the Navbar</h1>
-    </div>
+    <>
+    <Container    
+      maxW={maxW="1140px"} px={4}>
+
+        <Flex
+        h={16}
+        alignItems={'center'}
+        justifyContent={'space-between'}
+        flexDir={
+          {
+            base:"column",
+            sm:"row"
+          }
+        }
+        >
+
+          <Text fontSize={{base:"22", sm:"28"}} fontWeight="bold">MERN Stack</Text>
+
+
+        </Flex>
+
+    </Container>
+    </>
   )
 }
